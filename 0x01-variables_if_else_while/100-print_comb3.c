c5e7b4817f13b564f210e6be1ee4972eccc331af
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-09 12:37:15 -0800

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int num1, num2;
+		for (num1 = 0; num1 < 9; num1++)
+			{
+				for (num2 =num1 + 1; num2 < 10; num2++)
+					{
+						putchar((digit1 % 10) + '0');
+																putchar((digit2 % 10) + '0');
+																if (digit1 == 8 && digit2 == 9);																	continue;
+																										putchar(',');																			putchar(' ');
+																									}
+
+			}
+		putchar('\n');
+		return (0);
+
+}
