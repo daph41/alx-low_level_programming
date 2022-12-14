4f59dac210aaf9311053eece3aff0b796cf40d64
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-14 03:49:23 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ *
+ *  * File: 1-alphabet.c
+ *
+ *   * Auth: Daph moraa
+ *
+ *    */
+
+void print_alphabet(void)
+{		char letter;
+	
+		for (letter='a'; letter <='z'; letter++)
+			putchar(letter);
+		putchar ('\n');
+	
+}
+
