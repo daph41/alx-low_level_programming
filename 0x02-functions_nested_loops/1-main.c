0c58ce0b8214e7e98386faf6067ea188bc4f84c7
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-14 02:52:35 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -6,18 +6,17 @@
  *
  *  * main - check the code
  *
- *   *
+ *  *
  *
- *    * Return: Always 0.
+ *  * Return: Always 0.
  *
- *     */
+ */
 
 int main(void)
 
 {
+	print_alphabet();
 
-	    print_alphabet();
-
-	        return (0);
+        return (0);
 
 }
