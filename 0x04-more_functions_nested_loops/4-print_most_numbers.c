6fc3e868d72e2302e06bcaa6043cf451fe0fbf88
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-15 10:01:22 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,25 @@
 #include "main.h"
 
 void print_most_numbers(void)
+
 {
-	int num ='0';
 
-	for (num = 0; num <= 9; num++)
-	{
-		if (num != 2 && num != 4)
-			_putchar(num);
+		int num;
 
-	}
 
-	_putchar('\n');
+
+			for (num = 0; num <= 9; num++)
+
+					{
+
+								if (num != 2 && num != 4)
+
+												_putchar((num % 10) + '0');
+
+									}
+
+
+
+				_putchar('\n');
+
 }
