afe8b75e1eb14110b82b83e132a3498278a18e1e
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-19 12:50:07 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void randomPasswordGeneration(int N)
+
+{
+	int i = 0;
+	int randomizer = 0;
+	srand((unsigned int)(time(NULL)));
+
+
+}
