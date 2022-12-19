cff6ab6d3e5b99ba6ae5d0545e3dc9b6aad5ac0b
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-19 12:19:45 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_array(int *a, int n)
+
+{
+	int i = 0;
+
+	for (; i < n; i++)
+
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+
+	putchar('\n');
+}
