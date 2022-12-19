0cdec903294c3a416f627a07512ec1e31d3de4e6
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-19 12:37:38 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,42 @@
 #include"main.h"
 #include <stdio.h>
 
-int main(void)
+
+int _atoi(char *s)
 
 {
-	int a;
-	int b;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	a = 98;
-	b = 42;
+	while (s[c])
 
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+	{
+		if (s[c] == 45)
 
+		{
+			min *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+
+		}
+
+		if (isi == 1)
+
+		{
+			break;
+		}
+
+		c++;
+	}
+
+	ni *= min;
+	return (ni);
 }
