8a23204ffcfd1f537b2e786f0ea97af3ce4b014f
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2022-12-21 21:42:53 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *  * _strncat - a function that concatenates two strings.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+	src = start;
+
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+
+	*dest = '\0';
+	retuen(temp);
+}
