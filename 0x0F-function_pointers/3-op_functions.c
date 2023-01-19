0efa306eb4049f06a187977c39a3aa915a414302
Daph41 <daphinemoraa93@students.uonbi.ac.ke>
2023-01-19 02:32:07 -0800

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+//addition function
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+//subtraction function
+int op_sub(int a, int b)
+{
+	return (a-b);
+}
+
+//multiplication function
+int op_mul(int a, int b)
+{
+	return(a*b);
+}
+
+
+//division function
+int op_div(int a, int b)
+{
+	return (a/b);
+}
+
+//modulus function
+int op_mod(int a, int b)
+{
+	return (a%b);
+}
