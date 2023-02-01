31fe1029b001a83b3e3f8d695ae16e9b1a565a64
daph41 <daphinemoraa@gmail.com>
2023-02-01 02:16:24 -0800

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/* a function that frees list*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+
+
+}
